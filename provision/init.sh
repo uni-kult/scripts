6baee2ba4b66a0b0703417c708df139e29d4a5ef
uni-kult <147112152+uni-kult@users.noreply.github.com>
2025-02-17 15:54:13 +0100

    Create init.sh

---
+++ provision/init.sh
@@ -0,0 +1,46 @@
+set -euf -o pipefail
+set -x
+
+# apt-get update
+# apt-get install -y git sudo
+# git clone --quiet https://github.com/uni-kult/scripts /scripts
+# chown -R unikult:unikult  /scripts
+# usermod -aG sudo unikult
+# echo "unikult  ALL=NOPASSWD: ALL" | tee -a /etc/sudoers
+#
+# bash /scripts/provision/init.sh
+
+
+
+export DEBIAN_FRONTEND=noninteractive
+apt-get update && apt-get dist-upgrade
+DEBIAN_FRONTEND=noninteractive apt-get install -y git sudo curl wget zstd
+
+
+# setup dotfiles
+cd /home/unikult
+sudo -u unikult git clone --quiet https://git.sr.ht/~patrickhaussmann/dotfiles
+sudo -u unikult bash dotfiles/install.sh
+bash dotfiles/install.sh
+#set +x
+#password="$(/home/unikult/.bin/password)"
+#echo -e "root:${password}\nunikult:${password}" | tee ~/password | chpasswd
+#set -x
+#echo "" > /etc/motd
+#rm -f /etc/update-motd.d/10-uname
+#sudo -u unikult touch .hushlogin 
+
+
+cd /home/unikult
+bash dotfiles/.bin/update
+bash dotfiles/Install.Software/locale.sh
+bash dotfiles/Install.Software/ufw.sh
+bash dotfiles/Install.Software/fail2ban.sh
+#bash dotfiles/Install.Software/basic-tools.sh
+#bash dotfiles/Install.Software/security-hardening.sh
+bash dotfiles/Install.Software/docker.sh
+sudo usermod -aG docker unikult
+#sudo -u unikult newgrp docker #apply group information now
+
+mkdir /src
+chown -R unikult:unikult  /src
