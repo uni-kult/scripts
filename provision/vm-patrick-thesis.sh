dc1d58408e44f91169fae7da4ca9aca88e00d511
uni-kult <147112152+uni-kult@users.noreply.github.com>
2025-02-18 20:10:28 +0100

    Update vm-patrick-thesis.sh

--- provision/vm-patrick-thesis.sh
+++ provision/vm-patrick-thesis.sh
@@ -16,11 +16,11 @@ sudo ufw delete allow https
 
 
 curl -fsSL https://code-server.dev/install.sh | sh
-systemctl enable --now code-server@patrick
+sudo systemctl enable --now code-server@patrick
 sudo -u patrick mkdir -p /home/patrick/.config/code-server/
 cd /home/patrick
 echo -e "bind-addr: 127.0.0.1:8080\nauth: none\ncert: false" > .config/code-server/config.yaml
-chown patrick:patrick .config/code-server/config.yaml
+sudo chown patrick:patrick .config/code-server/config.yaml
 
 micro -plugin install wc
 micro -plugin install runit
@@ -43,12 +43,24 @@ sudo systemctl restart code-server@patrick
 sudo tailscale serve --bg 8080
 
 
+DEBIAN_FRONTEND=noninteractive sudo apt-get install -y perl wget
+wget -qO- "https://yihui.org/tinytex/install-bin-unix.sh" | sudo -u patrick sh
+echo 'export PATH="$HOME/bin:$PATH"' >> /home/patrick/.bashrc 
+cd /home/patrick
+sudo -u patrick /home/patrick/bin/tlmgr install babel-english babel-german german hyphen-german hyphen-english koma-script setspace parskip fancyhdr appendix placeins cite emptypage csqu
+otes microtype csvsimple pgf sidecap mhchem was siunitx caption titlesec floatrow minted upquote lineno oberdiek ulem datetime2 datetime2-german datetime2-english ragged2e
+
+
+
+echo "the rest of this script is not working :("
+exit 1
 
 read -p "Please enter your private key: " -s private_key
 cd /home/patrick
 echo "$private_key" | sudo tee .ssh/thesis_ed25519 > /dev/null
-chmod 600 .ssh/thesis_ed25519
-ssh-keygen -f .ssh/thesis_ed25519 -y > .ssh/thesis_ed25519.pub
+sudo chown patrick:patrick -R .ssh/thesis_ed25519
+sudo chmod 600 .ssh/thesis_ed25519
+sudo ssh-keygen -f .ssh/thesis_ed25519 -y > .ssh/thesis_ed25519.pub
 
 cat <<EOF | sudo tee .ssh/config > /dev/null
 Host github.com
@@ -58,18 +70,9 @@ Host github.com
     UserKnownHostsFile /dev/null
 EOF
 
-chown patrick:patrick -R .ssh/
-chmod 700 .ssh/
-
-
+sudo chown patrick:patrick -R .ssh/
+sudo chmod 700 .ssh/
 
 cd /src
 sudo -u patrick git clone --quiet git@github.com:PatrickHaussmann/masterthesis.git thesis
-
-DEBIAN_FRONTEND=noninteractive apt-get install -y perl wget
-wget -qO- "https://yihui.org/tinytex/install-bin-unix.sh" | sudo -u patrick sh
-echo 'export PATH="$HOME/bin:$PATH"' >> /home/patrick/.bashrc 
-cd /home/patrick
-sudo -u patrick /home/patrick/bin/tlmgr install babel-english babel-german german hyphen-german hyphen-english koma-script setspace parskip fancyhdr appendix placeins cite emptypage csqu
-otes microtype csvsimple pgf sidecap mhchem was siunitx caption titlesec floatrow minted upquote lineno oberdiek ulem datetime2 datetime2-german datetime2-english ragged2e
 
