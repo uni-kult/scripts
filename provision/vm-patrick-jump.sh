c5bed4489f26fd796f0c61be4be8c43c049dd4a8
uni-kult <147112152+uni-kult@users.noreply.github.com>
2025-03-04 22:46:56 +0100

    Create vm-patrick-jump.sh

---
+++ provision/vm-patrick-jump.sh
@@ -0,0 +1,18 @@
+set -eufx -o pipefail
+
+curl -fsSL https://tailscale.com/install.sh | sh
+
+echo 'net.ipv4.ip_forward = 1' | sudo tee -a /etc/sysctl.d/99-tailscale.conf
+echo 'net.ipv6.conf.all.forwarding = 1' | sudo tee -a /etc/sysctl.d/99-tailscale.conf
+sudo sysctl -p /etc/sysctl.d/99-tailscale.conf
+
+#NETDEV=$(ip -o route get 1.1.1.1 | cut -f 5 -d " ")
+#sudo ethtool -K $NETDEV rx-udp-gro-forwarding on rx-gro-list off
+
+printf '#!/bin/sh\n\nethtool -K %s rx-udp-gro-forwarding on rx-gro-list off \n' "$(ip -o route get 1.1.1.1 | cut -f 5 -d " ")" | sudo tee /etc/networkd-dispatcher/routable.d/50-tailscale
+sudo chmod 755 /etc/networkd-dispatcher/routable.d/50-tailscale
+sudo /etc/networkd-dispatcher/routable.d/50-tailscale
+test $? -eq 0 || echo 'An error occurred.'
+
+sudo tailscale up --advertise-routes=172.16.81.0/24 --ssh --advertise-exit-node
+
